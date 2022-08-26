2b31a5d30c06a29f1950c0786451f69e1cad34e7
Cindy Okino <cindy.okino@gmail.com>
2022-08-26 13:33:23 -0400

    business layer - add StudentService class

--- src/main/java/com/example/demo/student/StudentController.java
+++ src/main/java/com/example/demo/student/StudentController.java
@@ -4,24 +4,20 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.time.LocalDate;
-import java.time.Month;
 import java.util.List;
 
 @RestController
 @RequestMapping(path = "api/v1/student") // localhost:8080 --> localhost:8080/api/v1/student
 public class StudentController {
 
+    private final StudentService studentService;
+
+    public StudentController(StudentService studentService) {
+        this.studentService = studentService;
+    }
+
     @GetMapping
     public List<Student> getStudents() {
-        return List.of(
-                new Student(
-                        1L,
-                        "Mariam",
-                        "mariam.jamal@gmail.com",
-                        LocalDate.of(1993, Month.APRIL, 1),
-                        29
-                )
-        );
+        return studentService.getStudents();
     }
 }
---
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -0,0 +1,20 @@
+package com.example.demo.student;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+public class StudentService {
+
+    public List<Student> getStudents() {
+        return List.of(
+                new Student(
+                        1L,
+                        "Mariam",
+                        "mariam.jamal@gmail.com",
+                        LocalDate.of(1993, Month.APRIL, 1),
+                        29
+                )
+        );
+    }
+}
