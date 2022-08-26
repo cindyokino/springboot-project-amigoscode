55442fbf5aef25c49078d30b0de788b4f35f01ff
Cindy Okino <cindy.okino@gmail.com>
2022-08-26 14:03:08 -0400

    dependency injection - @Autowired and @Service

--- src/main/java/com/example/demo/student/StudentController.java
+++ src/main/java/com/example/demo/student/StudentController.java
@@ -1,5 +1,6 @@
 package com.example.demo.student;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,11 +8,12 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping(path = "api/v1/student") // localhost:8080 --> localhost:8080/api/v1/student
+@RequestMapping(path = "api/v1/student") // set URL: localhost:8080 --> localhost:8080/api/v1/student
 public class StudentController {
 
     private final StudentService studentService;
 
+    @Autowired // will instantiate StudentService class (new StudentService), then inject it into this constructor
     public StudentController(StudentService studentService) {
         this.studentService = studentService;
     }
