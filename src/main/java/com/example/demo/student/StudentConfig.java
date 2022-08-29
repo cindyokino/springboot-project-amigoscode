331baf8cf1dd18762cbe650401dd7c23c5e08a98
Cindy Okino <cindy.okino@gmail.com>
2022-08-29 14:46:40 -0400

    Add StudentConfig - save students into db

--- src/main/java/com/example/demo/student/StudentConfig.java
+++ src/main/java/com/example/demo/student/StudentConfig.java
@@ -1,2 +1,37 @@
-package com.example.demo.student;public class StudentConfig {
+package com.example.demo.student;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import static java.time.Month.JANUARY;
+
+@Configuration
+public class StudentConfig {
+
+    @Bean // use of @Bean so this code will run when the app is started
+    CommandLineRunner commandLineRunner(StudentRepository repository) { // CommandLineRunner = SpringBoot interface with a run method
+        return args -> {
+            Student mariam = new Student(
+                    "Mariam",
+                    "mariam.jamal@gmail.com",
+                    LocalDate.of(2000, JANUARY,5),
+                    21
+            );
+
+            Student alex = new Student(
+                    "Alex",
+                    "alex@gmail.com",
+                    LocalDate.of(2010, JANUARY,5),
+                    12
+            );
+
+            repository.saveAll(
+                    List.of(mariam, alex)
+            );
+        };
+    }
 }
